6b066310c9f9dd82632377ed904488d3bf462935
wuzs <wuzs0921@163.com>
2018-06-05 16:16:31 +0800

    首次提交

---
+++ src/main/java/com/wzs/git/gittest/GittestApplication.java
@@ -0,0 +1,12 @@
+package com.wzs.git.gittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GittestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GittestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/wzs/git/gittest/GittestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wzs.git.gittest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GittestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
